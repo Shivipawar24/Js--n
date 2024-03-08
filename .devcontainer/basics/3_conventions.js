410509f228ba3bf0f1bd0b10aaec53f42fc54892
Shivani pawar <91402735+Shivipawar24@users.noreply.github.com>
2024-03-08 12:42:38 +0000

    conversion

---
+++ .devcontainer/basics/3_conventions.js
@@ -0,0 +1,15 @@
+let score = null
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" = 33
+// 33abc = NaN
+// TRUE = 1, false = 0
+
+// 1 = true
+// "shivi" = true
+// 0 = false
